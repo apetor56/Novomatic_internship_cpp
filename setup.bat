aaf99b2b7a0ae1df4d93c5e534c45fafe37afee8
apetor56 <lukten.github@gmail.com>
2023-04-19 14:34:54 +0200

    set release build type

--- setup.bat
+++ setup.bat
@@ -2,7 +2,7 @@
 
 if not exist build (
     mkdir build
-    cmake -S . -B build -G "MinGW Makefiles"
+    cmake -S . -B build -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
 )
 
 cmake --build build 
